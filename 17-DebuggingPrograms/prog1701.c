45ef39431470be7fc405f989ba96c222453573f5
ron <rontekhne@users.noreply.github.com>
2021-03-22 18:58:45 -0300

    Finished chapter 17 - Debugging Programs.

---
+++ 17-DebuggingPrograms/prog1701.c
@@ -0,0 +1,25 @@
+/* prog1701. Adding debug statements with the preprocessor */
+
+#include <stdio.h>
+/* #define DEBUG */
+
+int process(int i, int j, int k)
+{
+	return i + j + k;
+}	
+
+int main(void)
+{
+	int i, j, k, nread;
+
+	nread = scanf("%i %i %i", &i, &j, &k);
+
+#ifdef DEBUG
+	fprintf(stderr, "Number of integers read = %i\n", nread);
+	fprintf(stderr, "i = %i, j = %i, k = %i\n", i, j, k);
+#endif
+
+	printf("%i\n", process(i, j, k));
+
+	return 0;
+}
